df85f99e70fb3f9ce320e116f0f7c1d079526e80
Saurabh <saurabh.d2106@gmail.com>
2025-04-09 12:22:06 +0530

    Reusable code modules

--- providers.tf
+++ providers.tf
@@ -4,11 +4,31 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "3.108.0"
     }
+
+    random = {
+      source = "hashicorp/random"
+      version = "3.7.1"
+    }
+
+    tls = {
+      source = "hashicorp/tls"
+      version = "4.0.6"
+    }
   }
+
+
 }
 
 provider "azurerm" {
   # Configuration options
   features {
   }
+}
+
+provider "random" {
+  
+}
+
+provider "tls" {
+  
 }
